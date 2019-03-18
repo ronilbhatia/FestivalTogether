0dfd8bd3f17a749553b5440f5faf1cbc7322c9a6
Ronil Bhatia <ronilbhatia@gmail.com>
2019-03-17 18:29:14 -0700

    Add config/keys file and setup connection to MongoDB in app.js

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB using Mongoose
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
